ca4499612165ee0626209f8b2886ad60a4635dfe
zhute <1114525038@qq.com>
2019-10-25 21:27:08 +0800

    第二次提交

---
+++ 201910.25/Java.java
@@ -0,0 +1,18 @@
+public class Java{
+	public static void main(String[] args){
+		long minLong = Long.MIN_VALUE;
+	    long maxLong = Long.MAX_VALUE;
+		System.out.println(minLong);
+	    System.out.println(maxLong);
+		short minShort = Short.MIN_VALUE;
+		short maxShort = Short.MAX_VALUE;
+		System.out.println(minShort);
+	    System.out.println(maxShort);
+		int a=3;
+		System.out.println(a);
+		int b=0b0010;
+		System.out.println(b);
+		long c=010L;
+		System.out.println(c);
+	}
+}
