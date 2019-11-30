57e86820c80185fcfdfb5d8077f62beae7a064f7
zhute <1114525038@qq.com>
2019-11-30 10:37:51 +0800

    数据结构

---
+++ 数据结构/Demo.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Demo{
+    public static void main(String[] args) {
+        Collection<String> list=new ArrayList<>();
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.add("我");
+        list.add("爱");
+        list.add("java");
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        Object[] array=list.toArray();
+        System.out.println(Arrays.toString(array));
+        for(String s:list){
+            System.out.println(s);
+        }
+        list.remove("爱");
+        for(String s:list){
+            System.out.println(s);
+        }
+        list.clear();
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+    }
+}
