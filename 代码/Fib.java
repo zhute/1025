c7e158d41c0f2a08a3bb3db5aba7df092a4ab39f
zhute <1114525038@qq.com>
2020-04-11 12:19:06 +0800

    斐波那契数

---
+++ 代码/Fib.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fib {
+    private static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        while (true) {
+            int n = Sc.nextInt();
+            System.out.println(fib(n));
+        }
+    }
+}
